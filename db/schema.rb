85aec7c9d42cf40802c9ddf4a690426dd5959082
Joe McCann <joe.mccann@mailbox.org>
2023-09-30 01:42:34 -0400

    Add replies to Comment model and parent_comment_id to comments table for future convenience. \n Allow creation of new comments via turbo_stream append.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_212609) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_165058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_212609) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_comment_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
