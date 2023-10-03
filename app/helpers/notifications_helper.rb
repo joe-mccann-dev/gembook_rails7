aafd218cfd583f13a12ddffddba65c05eef648c2
Joe McCann <joe.mccann@mailbox.org>
2023-10-03 17:32:48 -0400

    Remove id on dismiss notification button that was interferring with proper turbo stream replacement.

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,11 +1,13 @@
 module NotificationsHelper
 
-  def show_notification(notification)
+  def show_notification(notification, friendships)
     case notification.object_type
     when 'Friendship'
       render partial: 'friendship_notification', locals: { notification: notification }
     else
-      render partial: 'standard_notification', locals: { notification: notification }
+      turbo_frame_tag "dismiss-notification-#{notification.id}" do
+        render partial: 'standard_notification', locals: { notification: notification }
+      end
     end
   end
 end
