e44be8da6ea7a1e22db1bc9ab7d20866d88f8358
Joe McCann <joe.mccann@mailbox.org>
2023-10-04 16:59:43 -0400

    Update notification count bell in real time for standard notifications.

---
+++ app/javascript/controllers/notification_count_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  decrementNotificationCount() {
+    const notificationContainer = document.getElementById("notification-container");
+    const notificationCountElement = document.getElementById("notificationCount");
+    const numNotifications = parseInt(notificationCountElement.textContent);
+    console.log(numNotifications);
+    if (numNotifications === 1) {
+      notificationContainer.classList.remove('notifications-count');
+      notificationCountElement.remove();
+    } else {
+      notificationCountElement.textContent = numNotifications - 1;
+    }
+    
+  }
+}
