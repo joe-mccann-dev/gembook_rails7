32671d9ae5cae81de0dd4b965087a9f8ed2556c6
Joe McCann <joe.mccann@mailbox.org>
2023-10-04 15:57:06 -0400

    Show and hide post comments and comment replies via stimulus controller.

--- app/javascript/controllers/show_comments_controller.js
+++ app/javascript/controllers/show_comments_controller.js
@@ -2,9 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["commentForm"]
+  static targets = ["commentForm", "postComments", "commentComments", "postCommentsButton", "commentCommentsButton"];
 
   showCommentForm() {
     this.commentFormTarget.classList.toggle("hidden");
+  }
+
+  showPostComments() {
+    this.postCommentsTarget.classList.toggle("hidden");
+
+    if (this.postCommentsButtonTarget.textContent == "Show Comments") {
+      this.postCommentsButtonTarget.textContent = "Hide Comments";
+      this.postCommentsButtonTarget.insertAdjacentHTML('beforeend', '<ion-icon class="chevron" name="chevron-up-outline"></ion-icon>');
+    } else {
+      this.postCommentsButtonTarget.textContent = "Show Comments";
+      this.postCommentsButtonTarget.insertAdjacentHTML('beforeend', '<ion-icon class="chevron" name="chevron-down-outline"></ion-icon>')
+    }
+  }
+
+  showCommentComments() {
+    this.commentCommentsTarget.classList.toggle("hidden");
+
+    if (this.commentCommentsButtonTarget.textContent == "Show Replies") {
+      this.commentCommentsButtonTarget.textContent = "Hide Replies";
+      this.commentCommentsButtonTarget.insertAdjacentHTML('beforeend', '<ion-icon class="chevron" name="chevron-up-outline"></ion-icon>');
+    } else {
+      this.commentCommentsButtonTarget.textContent = "Show Replies";
+      this.commentCommentsButtonTarget.insertAdjacentHTML('beforeend', '<ion-icon class="chevron" name="chevron-down-outline"></ion-icon>')
+    }
   }
 } 
