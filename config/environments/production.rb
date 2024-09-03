b63eb6abd7e85ff8e2287ec65c45297beb387007
Joe McCann <joe.mccann@mailbox.org>
2024-09-03 15:50:19 -0400

    Modify production.rb--enable sendgrid; use aws for active storage.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,20 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # 
+      # default host for production
+  host = 'https://gembook.me'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: "apikey",
+    password: ENV['SENDGRID_API_KEY'],
+    domain: 'gembook.me',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -38,7 +52,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
